7274fe8914e20629aa4e2f3b88f2c95488a3253f
Kassim <kassimhuggins@macbook-pro.attlocal.net>
2021-04-10 11:59:36 -0700

    tag/deleteetc

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,28 +1,99 @@
 const router = require('express').Router();
 const { Tag, Product, ProductTag } = require('../../models');
+const { associations } = require('../../models/Tag');
+const { col } = require('sequelize/types');
 
 // The `/api/tags` endpoint
 
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  Tag.findAll({
+    include: [{
+      model: Product,
+      through: {
+        attributes: ['product_id', 'tag_id'],
+      }
+    }]
+  })
+  .then(dbData => res.json(dbData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
-router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
+// find a single tag by its `id`
   // be sure to include its associated Product data
+router.get('/:id', (req, res) => {
+  Tag.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [{
+      model: Product,
+      through: {
+        attributes: ['product_id', 'tag_id'],
+    },
+  }]
+})
+.then(dbData => res.json(dbData))
+.catch(err => {
+  console.log(err);
+  res.status(500).json(err);
 });
-
+});
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.create({
+    tag_name: req.body.tag_name,
+  })
+  .then(dbData => res.json(dbData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbData => {
+    if(!dbData[0]) {
+    res.status(404).json({ message: "No Tag found with this id"});
+    return;
+  }
+  res.json({ message: 'Tag updated'});
+  })
+.catch(err => {
+  console.log(err);
+  res.status(500).json(err);
+});
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbData => {
+    console.log(dbData)
+    if (!dbData) {
+      res.status(404).json({ message: 'No Tag found with this id '});
+      return;
+    }
+    res.json({ message: 'Tag deleted '});
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
